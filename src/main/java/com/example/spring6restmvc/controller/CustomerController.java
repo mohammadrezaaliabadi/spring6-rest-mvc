35da5023bd4972ce9e0407c613a363cc1b2fbaf0
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-08 18:36:52 +0330

    Assn Get Customer close 7

---
+++ src/main/java/com/example/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,30 @@
+package com.example.spring6restmvc.controller;
+
+import com.example.spring6restmvc.model.Customer;
+import com.example.spring6restmvc.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+@RestController
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @RequestMapping(value = "{customerId}",method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("customerId")UUID id){
+        return customerService.getCustomerById(id);
+    }
+
+}
---
+++ src/main/java/com/example/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,14 @@
+package com.example.spring6restmvc.services;
+
+import com.example.spring6restmvc.model.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+
+    Customer getCustomerById(UUID uuid);
+
+    List<Customer> getAllCustomers();
+
+}
