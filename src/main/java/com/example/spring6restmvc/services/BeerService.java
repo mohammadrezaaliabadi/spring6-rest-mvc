310b4e2c7fae6e51920401580bca4a4dd6428215
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-08 18:13:52 +0330

    List beer close #5

--- src/main/java/com/example/spring6restmvc/services/BeerService.java
+++ src/main/java/com/example/spring6restmvc/services/BeerService.java
@@ -6,9 +6,12 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
