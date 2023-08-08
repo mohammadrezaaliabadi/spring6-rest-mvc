8a826498478bfd6291c7a0ff1e7ee32a06ee5148
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-08 18:59:41 +0330

    Response header  close #10

--- src/main/java/com/example/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/example/spring6restmvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.example.spring6restmvc.model.Beer;
 import com.example.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,10 +21,14 @@ public class BeerController {
     private final BeerService beerService;
 
     @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
     public ResponseEntity handlePost(@RequestBody Beer beer){
         Beer  savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","/api/v1/beer/"+savedBeer.getId().toString());
+
+        return new ResponseEntity(headers,HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
