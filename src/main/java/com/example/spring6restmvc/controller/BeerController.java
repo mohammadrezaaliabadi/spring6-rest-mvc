dbc5cb7854fb89913789e3690d2e15e0b0485f68
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-08 16:14:18 +0330

    lombok logging close #3

--- src/main/java/com/example/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/example/spring6restmvc/controller/BeerController.java
@@ -1,11 +1,21 @@
 package com.example.spring6restmvc.controller;
 
+import com.example.spring6restmvc.model.Beer;
 import com.example.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class BeerController {
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id){
+        log.debug("Get beer by id - controller.");
+        return beerService.getBeerById(id);
+    }
 }
