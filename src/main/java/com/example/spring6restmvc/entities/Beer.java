81d79fa0e00e22c95cf6cb4a6f4fd3fcd1476043
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-10 16:02:59 +0330

    Adding Entities close #34

---
+++ src/main/java/com/example/spring6restmvc/entities/Beer.java
@@ -0,0 +1,29 @@
+package com.example.spring6restmvc.entities;
+
+import com.example.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+    @Id
+    private UUID id;
+    @Version
+    private Integer version;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
