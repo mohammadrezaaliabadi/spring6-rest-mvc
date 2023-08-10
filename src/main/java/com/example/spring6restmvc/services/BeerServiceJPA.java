fd38f841815c10265dd754ab4d0cb1f76cd20fb4
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-10 18:35:53 +0330

    JPA save new beer close #43

--- src/main/java/com/example/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/example/spring6restmvc/services/BeerServiceJPA.java
@@ -33,7 +33,7 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public BeerDTO saveNewBeer(BeerDTO beerDto) {
-        return null;
+        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
     }
 
     @Override
