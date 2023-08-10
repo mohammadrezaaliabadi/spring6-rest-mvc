7a69a8a0701a72e3410083e3c1fb18777343ecf2
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-10 18:28:34 +0330

    JPA customer get ops close #46

--- src/main/java/com/example/spring6restmvc/services/CustomerServiceJPA.java
+++ src/main/java/com/example/spring6restmvc/services/CustomerServiceJPA.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
 @Service
 @Primary
 @RequiredArgsConstructor
@@ -19,12 +21,15 @@ public class CustomerServiceJPA implements CustomerService {
     private final CustomerMapper customerMapper;
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID uuid) {
-        return Optional.empty();
+        return Optional.ofNullable(customerMapper
+                .customerToCustomerDto(customerRepository.findById(uuid).orElse(null)));
     }
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+        return customerRepository.findAll().stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
