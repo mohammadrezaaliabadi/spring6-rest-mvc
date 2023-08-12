4c3a8b054a6767bd40bbcfa6862bae7dce9bfef9
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-12 18:48:56 +0330

    Beer CSV fix integration tests close #81

--- src/test/java/com/example/spring6restmvc/Bootstrap/BootstrapDataTest.java
+++ src/test/java/com/example/spring6restmvc/Bootstrap/BootstrapDataTest.java
@@ -3,14 +3,17 @@ package com.example.spring6restmvc.Bootstrap;
 import com.example.spring6restmvc.repositories.BeerRepository;
 import com.example.spring6restmvc.repositories.CustomerRepository;
 import com.example.spring6restmvc.services.BeerCsvService;
+import com.example.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
     @Autowired
@@ -33,7 +36,7 @@ class BootstrapDataTest {
     void Testrun() throws Exception {
         bootstrapData.run();
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }
