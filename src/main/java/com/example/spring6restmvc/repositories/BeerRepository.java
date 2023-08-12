a058072f1967b9d6e13fa9dcad656485647c2e82
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-13 00:55:34 +0330

    Query spring data jpa close #87

--- src/main/java/com/example/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/com/example/spring6restmvc/repositories/BeerRepository.java
@@ -4,6 +4,8 @@ import com.example.spring6restmvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
+import java.util.List;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
