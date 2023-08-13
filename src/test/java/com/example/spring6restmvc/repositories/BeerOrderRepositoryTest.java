a25dc2f2e9843eca7b4cddaad3c04584c16076f1
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-13 19:39:44 +0330

    Rel one To one close pers #105

--- src/test/java/com/example/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/com/example/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.spring6restmvc.repositories;
 
 import com.example.spring6restmvc.entities.Beer;
 import com.example.spring6restmvc.entities.BeerOrder;
+import com.example.spring6restmvc.entities.BeerOrderShipment;
 import com.example.spring6restmvc.entities.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,6 +35,9 @@ class BeerOrderRepositoryTest {
         BeerOrder beerOrder = BeerOrder.builder()
                 .customerRef("Test order")
                 .customer(testCustomer)
+                .beerOrderShipment(BeerOrderShipment.builder()
+                        .trackingNumber("1235r")
+                        .build())
                 .build();
 
         BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
