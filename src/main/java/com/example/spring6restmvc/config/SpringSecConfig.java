a155aea1879b9de17f03da96b3eb34765d0389c7
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-17 18:30:13 +0330

    Resource server sec config close #114
    Resource server testing with postman close 115

--- src/main/java/com/example/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/example/spring6restmvc/config/SpringSecConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -13,13 +12,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecConfig {
 
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        //http.csrf(csrf->csrf.ignoringRequestMatchers("/api/**"))
+//
+//        http.csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(requests->requests.anyRequest().authenticated())
+//                .oauth2ResourceServer((oauth2)->oauth2.jwt(Customizer.withDefaults()));
+//        return http.build();
+//    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //http.csrf(csrf->csrf.ignoringRequestMatchers("/api/**"))
-
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requests->requests.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()
+                        )
+                );
         return http.build();
     }
+
+
 
 }
